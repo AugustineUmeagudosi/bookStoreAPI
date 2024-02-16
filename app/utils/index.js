a82ea7f8457fef3405db5d2f3e8e08a907fe5e7e
Austin <augustineumeagudosi@gmail.com>
2024-02-17 00:10:29 +0100

    removed unused files

--- app/utils/index.js
+++ app/utils/index.js
@@ -2,15 +2,9 @@
 import * as Helpers from './helpers';
 import ValidationHelper from './validation';
 import * as Response from './apiResponse';
-import * as Constants from './constants';
-import * as MailServices from './mailServices/mailServices';
-import * as Paystack from './paystack';
 
 export {
   Helpers,
   ValidationHelper,
-  Response,
-  Constants,
-  MailServices,
-  Paystack
+  Response
 };
